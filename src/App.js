65cf347ccf1ad8081f55c7a11a65b92afec9fac8
Danial Rezaei <daniyal.rezaii@gmail.com>
2020-02-24 12:05:49 +0330

    before eject

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import uuidv1 from 'uuid/v1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      newTodo: '',
+      filter: 'All',
+      data:[
+        {
+          status: false,
+          text: "1",
+          id: uuidv1()
+        },
+        {
+          status: false,
+          text: "2",
+          id: uuidv1()
+        },
+        {
+          status: false,
+          text: "3",
+          id: uuidv1()
+        },
+      ]
+    }
+  }
+  handleChange = (v) => {
+    this.setState({
+      newTodo: v.target.value
+    });
+  }
+  addTodo = ()=> {
+    this.setState({
+      data: [...this.state.data, {
+        status: false,
+        text: this.state.newTodo,
+        id: uuidv1()
+      }]
+    })
+  }
+  deleteTodo = (t) => {
+    this.setState({
+      data: this.state.data.filter(v => v.id != t)
+    })
+  }
+  changeTodo = (t) => {
+    this.setState({
+      data: this.state.data.map(v => {
+        if (v.id == t)
+          return {
+            text: v.text,
+            status: !v.status,
+            id: v.id
+          }
+
+        return v;
+      })
+    })
+    
+  }
+  filterTodo = (f) => {
+    this.setState({filter: f})
+  }
+  render() {
+    return ( 
+      <div>
+        <h1>TODOs</h1>
+        <div>{
+            ['All', 'Done', 'Undone'].map(v => {
+              return <button onClick={() => this.filterTodo(v)}>{v}</button>
+            })
+          }</div>
+        <ul>
+          {
+            this.state.data.filter(v => {
+              return this.state.filter == 'All' || (this.state.filter == 'Done' && v.status == 1) || (this.state.filter == 'Undone' && v.status == 0)
+            }).map(v => <li key={v.id}> <i>{v.status ? 'done' : 'undone'}</i> <input checked={v.status ? 'checked' : ''} onChange={() => this.changeTodo(v.id)} type="checkbox"></input> {v.text} <button onClick={() => this.deleteTodo(v.id)}>X</button></li>)
+          }
+        </ul>
+        <br />
+        <input onChange={this.handleChange} type="text" value={this.state.newTodo} />
+        <button onClick={this.addTodo}>ADD</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
